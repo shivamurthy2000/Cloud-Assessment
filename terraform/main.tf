407a75b1f88c4a0e2cb8e51932d35b86cdf9bcfd
shivamurthy2000 <157881346+shivamurthy2000@users.noreply.github.com>
2026-06-26 18:19:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-key_name = "terraform-key"
 resource "aws_instance" "web" {
   ami                         = "ami-0f58b397bc5c1f2e8"
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.deployer.key_name
+
+  key_name = "terraform-key"
 
   user_data = <<-EOF
 #!/bin/bash
