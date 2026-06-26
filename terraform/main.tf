0eb26214ad66f2dbb52f657a6a725ffa149c939f
shivamurthy2000 <sachinsachu18055@gmail.com>
2026-06-26 11:53:00 +0000

    Add Terraform infrastructure and GitHub Actions workflow

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "terraform-key"
+  public_key = file("${pathexpand("~/.ssh/aws-ec2-key.pub")}")
+}
+
+resource "aws_instance" "web" {
+  ami                         = "ami-0f58b397bc5c1f2e8"
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.deployer.key_name
+
+  user_data = <<-EOF
+#!/bin/bash
+apt update -y
+apt install -y apache2
+systemctl enable apache2
+systemctl start apache2
+rm -f /var/www/html/index.html
+echo "<h1>Terraform Working</h1>" > /var/www/html/index.html
+EOF
+
+  tags = {
+    Name = "cloud-assessment-server"
+  }
+}
---
+++ terraform/security.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "web_sg" {
+  name        = "cloud-assessment-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "cloud-assessment-sg"
+  }
+}
